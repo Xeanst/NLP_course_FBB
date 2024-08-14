d9c71019b0505224d3472fa08b5b4697dacc0289
Xeanst <93321097+Xeanst@users.noreply.github.com>
2024-08-14 13:07:01 +0300

    Add files via upload

---
+++ 07-08/memory_optimization.py
@@ -0,0 +1,61 @@
+def loss_backprop(generator, criterion, out, targets, normalize):
+    """
+    Memory optmization. Compute each timestep separately and sum grads.
+    """
+    assert out.size(1) == targets.size(1)
+    total = 0.0
+    out_grad = []
+    for i in range(out.size(1)):
+        out_column = Variable(out[:, i].data, requires_grad=True)
+        gen = generator(out_column)
+        loss = criterion(gen, targets[:, i]) / normalize
+        total += loss.item() #.data[0]
+        loss.backward()
+        out_grad.append(out_column.grad.data.clone())
+    out_grad = torch.stack(out_grad, dim=1)
+    out.backward(gradient=out_grad)
+    return total
+
+def make_std_mask(src, tgt, pad):
+    src_mask = (src != pad).unsqueeze(-2)
+    tgt_mask = (tgt != pad).unsqueeze(-2)
+    tgt_mask = tgt_mask & Variable(subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
+    return src_mask, tgt_mask
+
+def train_epoch(train_iter, model, criterion, opt, transpose=False):
+    model.train()
+    for i, batch in enumerate(train_iter):
+        src, trg, src_mask, trg_mask = \
+                batch.src.to(device), batch.trg.to(device), batch.src_mask.to(device), batch.trg_mask.to(device)
+        out = model.forward(src, trg[:, :-1], src_mask, trg_mask[:, :-1, :-1])
+        loss = loss_backprop(model.generator, criterion, out, trg[:, 1:], batch.ntokens)
+
+        model_opt.step()
+        model_opt.optimizer.zero_grad()
+        if i % 10 == 1:
+            print(i, loss, model_opt._rate)
+
+def valid_epoch(valid_iter, model, criterion, transpose=False):
+    model.eval()
+    total = 0
+    for batch in valid_iter:
+        src, trg, src_mask, trg_mask = \
+            batch.src.to(device), batch.trg.to(device), batch.src_mask.to(device), batch.trg_mask.to(device)
+        out = model.forward(src, trg[:, :-1], src_mask, trg_mask[:, :-1, :-1])
+        loss = loss_backprop(model.generator, criterion, out, trg[:, 1:], batch.ntokens)
+
+class Batch:
+    def __init__(self, src, trg, src_mask, trg_mask, ntokens):
+        self.src = src
+        self.trg = trg
+        self.src_mask = src_mask
+        self.trg_mask = trg_mask
+        self.ntokens = ntokens
+
+def data_gen(V, batch, nbatches):
+    for i in range(nbatches):
+        data = torch.from_numpy(np.random.randint(1, V, size=(batch, 10)))
+        src = Variable(data, requires_grad=False)
+        tgt = Variable(data, requires_grad=False)
+        src_mask, tgt_mask = make_std_mask(src, tgt, 0)
+        yield Batch(src, tgt, src_mask, tgt_mask, (tgt[1:] != 0).data.sum())
